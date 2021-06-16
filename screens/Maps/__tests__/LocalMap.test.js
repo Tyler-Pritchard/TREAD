afb394f82671d46a3a2c565d9afc400703896ea0
Tyler Pritchard <pritchard.tyler@gmail.com>
2021-06-16 01:04:59 -0700

    init test

--- screens/Maps/__tests__/LocalMap.test.js
+++ screens/Maps/__tests__/LocalMap.test.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import {render} from '@testing-library/react-native';
-import Maps from "./index.js";
 
-it ('renders default elements', () => {
-    const { getAllByText } = render(<Maps />);
+test.only('this will be the only test that runs', () => {
+    expect(true).toBe(true);
+  });
+  
+  test('this test will not run', () => {
+    expect('A').toBe('A');
+  });
 
-    getAllByText('Maps').length(); 
 
-})
 
 //it renders default elements
 //it renders marker1
 //it handles valid input submission
 //it renders marker2
 //it renders polyline
+
+// Given - some precondition
+// When - some action executed by the function that you’re testing
+// Then - the expected outcome
+
+//Add detox later for e2e and device simulation testing
